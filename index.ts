f671e3a85ca315cc09127019e4a05b8510c5f6dd
mzhubail <mzhubail@gmail.com>
2025-02-07 23:24:32 +0300

    Read argv + read config

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hi!");
+import { readFileSync } from "fs";
+import { parse } from "yaml";
+
+function main() {
+  const filename = process.argv[2];
+  if (!filename) {
+    console.error("No filename was provided");
+    process.exit(-1);
+  }
+
+  const fileContent = readFileSync(filename);
+  const config = parse(fileContent.toString()) as unknown;
+}
+
+main();
