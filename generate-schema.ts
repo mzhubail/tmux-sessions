653824cff7ac6a1e9f517a556f502d18b8605de4
mzhubail <mzhubail@gmail.com>
2025-02-08 00:02:13 +0300

    Generate schema for use with editor

---
+++ generate-schema.ts
@@ -0,0 +1,6 @@
+import { configSechma } from "config-schema";
+import { writeFileSync } from "fs";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
+const jsonSchema = zodToJsonSchema(configSechma);
+writeFileSync("schema.json", JSON.stringify(jsonSchema));
