d6b04d7d360da0d913b4a670439a4cf0fc0c70d1
mzhubail <mzhubail@gmail.com>
2025-02-08 11:14:12 +0300

    Allow passing in `undefined` values for convenience

--- src/run-tmux.ts
+++ src/run-tmux.ts
@@ -1,8 +1,10 @@
 import { tokenizeArgs } from "args-tokenizer";
 import { x, type Output } from "tinyexec";
 
-export async function runTmux(args: string[] | string) {
-  const castedArgs = typeof args === "string" ? tokenizeArgs(args) : args;
+export async function runTmux(args: (string | undefined)[] | string) {
+  const castedArgs = (
+    typeof args === "string" ? tokenizeArgs(args) : args
+  ).filter((v): v is string => !!v);
 
   const output = process.env.TEST
     ? await x("echo", ["tmux", ...castedArgs])
